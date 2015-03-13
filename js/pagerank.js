5fa1f5cc77e408682a0ea9b672d47c9b5e03607e
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2015-03-13 23:20:22 +0900

    create demo page

---
+++ js/pagerank.js
@@ -0,0 +1,123 @@
+// pagerank.js
+//
+// based on networkx.pagerank 1.9.1 (Python)
+// https://github.com/networkx/networkx/blob/master/networkx/algorithms/link_analysis/pagerank_alg.py
+
+function pagerank(G, params) {
+
+  // set default parameters
+  if (params == null) params = {};
+  if (params.alpha == null) params.alpha = 0.85;
+  if (params.tol == null) params.tol = 1.0e-6;
+  if (params.max_iter == null) params.max_iter = 100;
+
+  // tools
+  var sum = function(xs) {
+    return xs.reduce(function(prev, current, idx, xs) {
+      return prev + current;
+    });
+  };
+
+  var N = Object.keys(G).length
+  if (N == 0) return {};
+
+  // Create a copy in (right) stochastic form
+  var W = {}
+  for (var node in G) {
+    var nodes = G[node];
+    var num_nodes = nodes.length;
+    var new_nodes = {};
+    //for (var node_ in nodes) {
+    nodes.forEach(function(node_) {
+      new_nodes[node_] = {'weight': 1.0 / num_nodes};
+    });
+    W[node] = new_nodes;
+  }
+  //console.log(W);
+
+  // Choose fixed starting vector if not given
+  var x = {};
+  if (params.nstart == null) {
+    for (var node in W) x[node] = 1.0 / N;
+  }
+  else {
+    // Normalized nstart vector
+    var sum_ = sum(Object.values(params.nstart));
+    for (var node in params.nstart)
+      x[node] = params.nstart[node] / sum_;
+  }
+  //console.log(x);
+
+  p = {};
+  if (params.personalization == null) {
+    // Assign uniform personalization vector if not given
+    for (var node in W) p[node] = 1.0 / N;
+  }
+  else {
+    // TODO: check missing nodes
+    var sum_ = sum(Object.values(params.personalization));
+    for (var node in params.personalization)
+      p[node] = params.personalization[node] / sum_;
+  }
+
+  var dangling_weights = {};
+  if (params.dangling == null) {
+    // Use personalization vector if dangling vector not specified
+    dangling_weights = p;
+  }
+  else {
+    // TODO: check missing nodes
+    var sum_ = sum(Object.values(params.dangling));
+    for (var node in params.dangling)
+      dangling_weights[node] = params.dangling[node] / sum_;
+  }
+  var dangling_nodes = [];
+  for (var node in W) {
+    if (Object.keys(W[node]).length == 0) // need review
+      dangling_nodes.push(node);
+  }
+  //console.log(dangling_nodes);
+
+  // power iteration: make up to max_iter iterations
+  for (var iter_count = 0; iter_count < params.max_iter; iter_count++) {
+    var xlast = x;
+    x = {};
+    for (var node in xlast) x[node] = 0.0;
+    var sum_ = 0.0;
+    dangling_nodes.forEach(function(node) {
+      sum_ += xlast[node]
+    });
+    var danglesum = params.alpha * sum_;
+
+    for (var node in x) {
+      // this matrix multiply looks odd because it is
+      // doing a left multiply x^T=xlast^T*W
+      for (var nbr in W[node]) {
+        x[nbr] += params.alpha * xlast[node] * W[node][nbr]['weight'];
+      }
+      x[node] += danglesum * dangling_weights[node] + (1.0 - params.alpha) * p[node];
+    }
+
+    // check convergence, l1 norm
+    var err = 0.0;
+    Object.keys(x).forEach(function(node) {
+      err += Math.abs(x[node] - xlast[node])
+    });
+    if (err < N*params.tol)
+      return x;
+  }
+
+  console.warn('pagerank: power iteration failed to converge in ' +
+               params.iter_max + ' iterations.');
+  return x;
+}
+
+// node.js
+//module.exports = pagerank;
+
+// test
+//var scores = pagerank({1: [2, 3, 4], 2: [3], 3: [4], 4: []}, {alpha: 0.9});
+//console.log(scores);
+//
+//var scores = pagerank({'foo': ['bar', 'hoge'], 'bar': [], 'hoge': ['foo']});
+//console.log(scores);
---
+++ js/summarize.js
@@ -0,0 +1,55 @@
+function summarize() {
+
+  var text = document.getElementById('input').value;
+  var num_sent = document.getElementById('num_sent').value;
+  //console.log(text);
+  //console.log(num_sent);
+
+  var sent_splitter = function(text) {
+    return text.replace(/([。．？！]+)/g, '$1|').split('|');
+  };
+
+  // https://sites.google.com/site/kevinbouge/stopwords-lists
+  var _stopwords = 'これ それ あれ この その あの ここ そこ あそこ こちら どこ だれ なに なん 何 私 貴方 貴方方 我々 私達 あの人 あのかた 彼女 彼 です あります おります います は が の に を で え から まで より も どの と し それで しかし';
+  var stopwords = {};
+  _stopwords.split(' ').forEach(function(w) { stopwords[w] = true; });
+
+  var _segmenter = new TinySegmenter();
+  var word_segmenter = function(sent) {
+    return _segmenter.segment(sent)
+      .map(function(w) { return w.trim(); })
+      .filter(function(w) {
+        return (stopwords[w] == null && w.length > 0 &&
+                ! /^[１２３４５６７８９０！＠＃＄％＾＆＊（）ー＝＋｜〜：；「」’”，、。，．＜＞\d!@#$%\^&*()\-=+\|~\[\]'":;,.?/<>]+$/.test(w));
+      });
+  }
+
+  var sentences = sent_splitter(text);
+  var result = lexrank(sentences, {word_segmenter: word_segmenter})
+                 .slice(0, num_sent);
+  //console.log(result);
+  var score_max = result[0].score;
+  var score_min = result.slice(-1)[0].score;
+  result.sort(function(a, b) { return a.idx - b.idx; });
+
+  var score2color = function(s) { // need review
+    // [0,1] -> [6,0] -> #nnnnnn
+    var c = '#';
+    var n = Math.floor((1 - s) * 6);
+    for (var i = 0; i < 6; i++) c += n;
+    return c;
+  }
+
+  var summary = '';
+  result.forEach(function(meta) {
+    //console.log('(' + meta.score + ', ' + meta.idx + ')  ' + sentences[meta.idx]);
+    var score_norm = (meta.score - score_min) / (score_max - score_min);
+    //console.log(meta.score + ' ' + score_norm);
+    var color = score2color(score_norm);
+    //console.log(color);
+    summary += '<span style="color:' + color + '">' + sentences[meta.idx] + '</span>';
+  });
+  document.getElementById('output').innerHTML = '';
+  document.getElementById('output').innerHTML =
+    '<div class="jumbotron"><p>' + summary + '<p></div>';
+}
